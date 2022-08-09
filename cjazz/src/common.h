43018396691edc67e2b15e79233893f0d7507b66
Patrik Palmér <patrik.palmer@gmail.com>
2022-08-09 09:10:11 +0200

    Add test chunk

---
+++ cjazz/src/common.h
@@ -0,0 +1,8 @@
+#ifndef jazz_common_h
+#define jazz_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
---
+++ cjazz/src/memory.h
@@ -0,0 +1,17 @@
+#ifndef jazz_memory_h
+#define jazz_memory_h
+
+#include "common.h"
+
+#define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
+    (type*)reallocate(pointer, sizeof(type) * (oldCount), \
+    sizeof(type) * (newCount))
+
+#define FREE_ARRAY(type, pointer, oldCount) \
+    reallocate(pointer, sizeof(type) * (oldCount), 0)
+
+void* reallocate(void* previous, size_t oldSize, size_t newSize);
+
+#endif
